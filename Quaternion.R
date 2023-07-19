fce3f41f335c16a3c49052f3373324369ee630ad
SawyersCoding <scstanley@mta.ca>
2023-07-19 09:59:54 -0300

    Create Quaternion.R

---
+++ Quaternion.R
@@ -0,0 +1,118 @@
+
+quaternion <- function(x, y, z, w){
+  return(c(x, y, z, w))  
+}
+
+eulerZXZToQuat <- function(p1,P,p2){
+  if(P == 0){
+    return(c(0,0,0,0))
+  }
+  thetaPlus <- (p2 + p1)/2
+  thetaMinus <- (p2 - p1)/2
+  cP <- cos(P)
+  ttP <- tan(thetaPlus)
+  ttM <- tan(thetaMinus)
+  
+  # Calculate quaternion values
+  w <- sqrt((cP + 1) / (2 * (1 + ttP * ttP)))
+  x <- sqrt((1 - cP) / (2 * (1 + ttM * ttM)))
+  y <- x * ttM
+  z <- w * ttP
+  return(quatNormalized(quaternion(x,y,z,w)))
+}
+
+quatToEulerInDegreesZXZ <- function(quat){
+  x <- 1
+  y <- 2
+  z <- 3
+  w <- 4
+  if(quat[x] == 0 && quat[y] == 0 && quat[z] == 0 && quat[w] == 0) {
+    return(c(0,0,0))
+  }
+  
+  i <- 3
+  j <- 1
+  k <- 2
+  
+  a <- quat[w]
+  b <- quat[z]
+  c <- quat[x]
+  d <- quat[y]
+  
+  theta1 <- 0
+  theta2 <- acos(2 * ((a*a+b*b)/(a*a+b*b+c*c+d*d)) - 1)
+  theta3 <- 0
+  thetaPlus <- atan2(b, a)
+  thetaMinus <- atan2(d, c)
+  
+  if(theta2 == 0){
+    theta1 <- 0
+    theta3 <- 2 * thetaPlus
+  }
+  else if(theta2 == pi/2){
+    theta1 <- 0
+    theta3 <- 2 * thetaMinus
+  }
+  else{
+    theta1 = thetaPlus - thetaMinus
+    theta3 <- thetaPlus + thetaMinus
+  }
+  
+  while(theta1 >= 2*pi) {
+    theta1 <- theta1 - 2*pi
+  }
+  while(theta2 >= 2*pi) {
+    theta2 <- theta2 - 2*pi
+  }
+  while(theta3 >= 2*pi) {
+    theta3 <- theta3 - 2*pi
+  }
+  while(theta1 < 0) {
+    theta1 <- theta1 + 2*pi
+  }
+  while(theta2 < 0) {
+    theta2 <- theta2 + 2*pi
+  }
+  while(theta3 < 0) {
+    theta3 <- theta3 + 2*pi
+  }
+  result <- c(theta1, theta2, theta3) * (180 / pi)
+  return(result)
+}
+
+quatMultiply <- function(q1, q2){
+  x <- 1
+  y <- 2
+  z <- 3
+  w <- 4
+  
+  quat <- quaternion(0,0,0,0)
+  quat[w] <- q1[w] * q2[w] - q1[x] * q2[x] - q1[y] * q2[y] - q1[z] * q2[z]
+  quat[x] <- q1[w] * q2[x] + q1[x] * q2[w] + q1[y] * q2[z] - q1[z] * q2[y]
+  quat[y] <- q1[w] * q2[y] - q1[x] * q2[z] + q1[y] * q2[w] + q1[z] * q2[x]
+  quat[z] <- q1[w] * q2[z] + q1[x] * q2[y] - q1[y] * q2[x] + q1[z] * q2[w]
+  
+  return(quat)
+}
+
+quatNormalized <- function(quat){
+  x <- quat[1]
+  y <- quat[2]
+  z <- quat[3]
+  w <- quat[4]
+  
+  magnitude <- sqrt(x*x+y*y+z*z+w*w)
+  quat[1] = quat[1] / magnitude
+  quat[2] = quat[2] / magnitude
+  quat[3] = quat[3] / magnitude
+  quat[4] = quat[4] / magnitude
+  return(quat)
+}
+
+quatConjugate <- function(quat){
+  x <- 1
+  y <- 2
+  z <- 3
+  w <- 4
+  return(quaternion(-quat[x], -quat[y], -quat[z], quat[w]))
+}
